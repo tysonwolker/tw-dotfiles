325a5ef2f24023a6ede112a07807cf32f3c24e61
Tyson Wolker <tyson.wolker@WHALOSX1AUVG8WN.local>
2016-03-25 23:56:07 +1100

    Added paths for go and nvm

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,7 +1,18 @@
+nvm_dir=$HOME/.nvm
+go_path=/Users/$USER/go/
+
 # path, the 0 in the filename causes this to load first
 path=(
-  $path
-  $HOME/.yadr/bin
-  $HOME/.yadr/bin/yadr
+	$path
+	$HOME/.yadr/bin
+	$HOME/.yadr/bin/yadr
+  $GOPATH/bin
 )
+#RVM
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"·
 
+# NVM
+source $(brew --prefix nvm)/nvm.sh
+
+# Go Programming
+export PATH=$GOPATH/bin:$PATH

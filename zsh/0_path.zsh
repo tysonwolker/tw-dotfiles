c26d9304dd5248d667f78d62893f718b1ee5c95f
Tyson Wolker <tyson.wolker@WHALOSX1AUVG8WN.local>
2016-03-26 23:24:40 +1100

    Finally fixed rvm issue

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,13 +1,17 @@
+# NVM
+source $(brew --prefix nvm)/nvm.sh
+
+# RVM
+[ -s ${HOME}/.rvm/scripts/rvm ] && source ${HOME}/.rvm/scripts/rvm
+
 NVM_DIR=$HOME/.nvm
 GOPATH=/Users/$USER/go/
-
 # path, the 0 in the filename causes this to load first
 path=(
+  $GEM_HOME/bin
+  $HOME/.rvm/bin
 	$path
 	$HOME/.yadr/bin
 	$HOME/.yadr/bin/yadr
   $GOPATH/bin
 )
-
-# NVM
-source $(brew --prefix nvm)/nvm.sh

41d6f71cb152d4f3761adcd51cc02dca3996fd81
Tyson Wolker <tysonwolker@gmail.com>
2016-05-30 14:35:16 +1000

    Added force option to shell script

--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+function removeInstallation {
+	echo "Removing existing YADR installation"
+	rm -rf ~/.yadr
+}
+
+while getopts ":f" opt; do
+  case "$opt" in
+    f)
+      removeInstallation
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
     git clone --depth=1 https://github.com/tysonwolker/tw-dotfiles.git "$HOME/.yadr"
